dfd785d6343f474a10cd079dbfc4e3431387deed
alecnavapanich <alecnavapanich@gmail.com>
2026-02-01 01:01:06 -0800

    Main Menu Screen
    Added main menu
    -TODO add sounds and function to buttons

--- MaskGame/Assets/Scripts/GameOverScreen.cs
+++ MaskGame/Assets/Scripts/GameOverScreen.cs
@@ -8,9 +8,11 @@ public class GameOverScreen : MonoBehaviour
         // Stretch goal -> save progress and reset to last room
         // currently resets the game
         SceneManager.LoadScene("Game");
+        Debug.Log("LastRoomButton called successfully!");
     }
     public void ExitButton()
     {
         SceneManager.LoadScene("MainMenu");
+        Debug.Log("ExitButton called successfully!");
     }
 }
---
+++ MaskGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        Debug.Log("StartButton called successfully!");
+        SceneManager.LoadScene("Game");
+    }
+    public void SettingsButton()
+    {
+        Debug.Log("SettingsButton called successfully!");
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("ExitButton called successfully!");
+        Application.Quit();
+    }
+}
